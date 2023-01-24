08fc41090f829932b39cf48938e91f9fd72cb492
Toby Cobbold <tobycobbold@hotmail.com>
2023-01-24 02:00:25 +0000

    Code changes

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+import requests
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Test 123"}
+    return {"message": "Test 12345"}
+
+@app.get("/httpbin")
+async def httpbin():
+    return requests.get("https://httpbin.org/uuid").json().get("uuid", "Uh oh.")
